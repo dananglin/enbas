7fd93e877829808bc7479d9805b65e81b3489b54
Dan Anglin <daangling@gmail.com>
2024-08-17 19:15:12 +0100

    ci: add a workflow for tests
    - Added an action for running mage targets
    - Added a workflow for running tests
    - Add our first unit test
    
    PR: https://codeflow.dananglin.me.uk/apollo/enbas/pulls/52

---
+++ internal/utilities/utilities_test.go
@@ -0,0 +1,38 @@
+package utilities_test
+
+import (
+	"testing"
+
+	"codeflow.dananglin.me.uk/apollo/enbas/internal/utilities"
+)
+
+func TestGetFQDN(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		instance string
+		want     string
+	}{
+		{
+			instance: "https://gts.red-crow.private",
+			want:     "gts.red-crow.private",
+		},
+		{
+			instance: "http://gotosocial.yellow-desert.social",
+			want:     "gotosocial.yellow-desert.social",
+		},
+		{
+			instance: "fedi.blue-mammoth.party",
+			want:     "fedi.blue-mammoth.party",
+		},
+	}
+
+	for _, c := range cases {
+		got := utilities.GetFQDN(c.instance)
+		if c.want != got {
+			t.Errorf("Unexpected result: want %s, got %s", c.want, got)
+		} else {
+			t.Logf("Expected result: got %s", got)
+		}
+	}
+}
